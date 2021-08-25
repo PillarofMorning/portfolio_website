c5a847762f46dafabd5ac8f341d24a1cc991c3d5
AntoAvestruz <gtzephr@gmail.com>
2021-08-25 13:46:43 -0500

    portfolio_website.iml

---
+++ portfolio_website/portfolio/home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, HttpResponse
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def resume(request):
+    return render(request, 'resume.html')
